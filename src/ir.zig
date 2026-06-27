05ca97556b5b3e8ad3ecdbad30a2d8d57c1060d1
Robby Sarvis <rjs.app.dev@proton.me>
2026-06-26 22:50:39 -0500

    Initial pass building from our profiles_resource and profiles_types json files

---
+++ src/ir.zig
@@ -0,0 +1,59 @@
+// TODO:: set description field on needed fields
+// TODO:: make sure pattern is set where it's available
+
+pub const FhirType_Primitive = struct {
+    pattern: ?[]const u8,
+    name: []const u8,
+    type: []const u8,
+};
+
+pub const FhirType_Structure = struct {
+    name: []const u8,
+    description: ?[]const u8,
+    fields: []FhirField,
+    is_resource: bool,
+};
+
+pub const FhirType_Enumeration = struct {
+    name: []const u8,
+    description: ?[]const u8,
+    variants: []const []const u8,
+};
+
+pub const FhirType_OneOf = struct {
+    name: []const u8,
+    refs: []const []const u8,
+};
+
+pub const FhirField = struct {
+    name: []const u8,
+    description: ?[]const u8,
+    type_ref: FieldType,
+    is_optional: bool,
+    is_slice: bool,
+    is_boxed: bool = false,
+    min: u32 = 0,
+    max: ?u32 = null, // null = unbounded
+};
+
+pub const FieldType = union(enum) {
+    ref: []const u8,
+    primitive: []const u8,
+    inline_enum: []const []const u8,
+    choice: []const ChoiceOption,
+};
+
+pub const ChoiceOption = struct { suffix: []const u8, typeRef: FieldType };
+
+pub const FhirType = union(enum) {
+    primitive: FhirType_Primitive,
+    structure: FhirType_Structure,
+    enumeration: FhirType_Enumeration,
+    oneOf: FhirType_OneOf,
+};
+
+pub const FhirIntermediateRepresentationError = error{
+    MissingDefinitions,
+    InvalidFormat,
+    OutOfMemory,
+};
