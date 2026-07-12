f66ea599a04c4f07a002c684843919a97d7acb0e
Robby Sarvis <rjs.app.dev@proton.me>
2026-07-12 16:28:38 -0500

    prep for merge to main, remove old code and some other changes, build confirmation

--- src/main.zig
+++ src/main.zig
@@ -5,20 +5,21 @@ const fhir_zig = @import("fhir_zig");
 
 // copy to src file I'm lazy and haven't made this not a sharp pointy edge... yet
 // const generatedFhir = @import("./fhir_r4.zig");
+// const generatedFhir = @import("./fhir_r5.zig");
 
 pub fn main(init: std.process.Init) !void {
     const arena: std.mem.Allocator = init.arena.allocator();
 
-    // try fhir_zig.emitFhirR4Types(arena, init.io);
-    // try fhir_zig.emitFhirR5Types(arena, init.io);
+    try fhir_zig.emitFhirR4Types(arena, init.io);
+    try fhir_zig.emitFhirR5Types(arena, init.io);
 
-    try fhir_zig.testRepFromBundle(arena, init.io);
+    // _ = arena;
 
     // const name: generatedFhir.HumanName = .{
-    //     .family = "Test",
-    //     .given = &.{"Mc"},
+    //     .family = .{ .value = "Test" },
+    //     .given = &.{.{ .value = "Mc" }},
     // };
-    // const patient: generatedFhir.Patient = .{ .active = true, .name = &.{name} };
+    // const patient: generatedFhir.Patient = .{ .active = .{ .value = true }, .name = &.{name} };
 
     // _ = patient;
 }
--- src/root.zig
+++ src/root.zig
@@ -58,7 +58,7 @@ const emitter = @import("emitter.zig");
 //     try emitFhirTypes(arena, initIo, inputPath, "fhir_r5.zig");
 // }
 
-pub fn testRepFromBundle(arena: std.mem.Allocator, initIo: Io) !void {
+pub fn emitFhirR4Types(arena: std.mem.Allocator, initIo: Io) !void {
     const start = std.Io.Clock.now(.awake, initIo);
 
     const cwd = std.Io.Dir.cwd();
@@ -108,7 +108,71 @@ pub fn testRepFromBundle(arena: std.mem.Allocator, initIo: Io) !void {
     var output_dir: std.Io.Dir = try cwd.openDir(initIo, "output", .{});
     defer output_dir.close(initIo);
 
-    const irFile: std.Io.File = try output_dir.createFile(initIo, "fhir_r4-dont-trust.zig", .{});
+    const irFile: std.Io.File = try output_dir.createFile(initIo, "fhir_r4.zig", .{});
+    defer irFile.close(initIo);
+
+    var file_writer = irFile.writer(initIo, &.{});
+    const writerZigOut = &file_writer.interface;
+
+    const byte_written = try writerZigOut.write(emitted);
+    std.debug.print("Successfully wrote {d} bytes.\n", .{byte_written});
+
+    const end = std.Io.Clock.now(.awake, initIo);
+    const duration = start.durationTo(end);
+    std.debug.print("Elapsed time: {} ms\n", .{duration.toMilliseconds()});
+}
+
+pub fn emitFhirR5Types(arena: std.mem.Allocator, initIo: Io) !void {
+    const start = std.Io.Clock.now(.awake, initIo);
+
+    const cwd = std.Io.Dir.cwd();
+    const dir = try cwd.openDir(initIo, ".", .{ .iterate = true });
+    defer dir.close(initIo);
+
+    const file = try dir.openFile(initIo, "schemas/schema-json-r5/profiles-resources.json", .{});
+    defer file.close(initIo);
+
+    const stat = try file.stat(initIo);
+
+    var buffer = try arena.alloc(u8, stat.size);
+    var file_reader = file.reader(initIo, buffer);
+
+    const reader = &file_reader.interface;
+    try reader.readSliceAll(buffer);
+
+    const resourcesParsed = try std.json.parseFromSlice(std.json.Value, arena, buffer, .{});
+
+    const typesFile = try dir.openFile(initIo, "schemas/schema-json-r5/profiles-types.json", .{});
+    defer typesFile.close(initIo);
+    const typesStat = try typesFile.stat(initIo);
+
+    buffer = try arena.alloc(u8, typesStat.size);
+    file_reader = typesFile.reader(initIo, buffer);
+    const typesReader = &file_reader.interface;
+    try typesReader.readSliceAll(buffer);
+
+    const typesParsed = try std.json.parseFromSlice(std.json.Value, arena, buffer, .{});
+
+    const fhirTypesArr = try ir.buildIntermediateRepresentationFromBundles(arena, &[_]std.json.Parsed(std.json.Value){ resourcesParsed, typesParsed });
+
+    const out_file = try dir.createFile(initIo, "ir-rep.debug.json", .{});
+    defer out_file.close(initIo);
+
+    var out_buffer: [4096]u8 = undefined;
+    var out_writer = out_file.writer(initIo, &out_buffer);
+    const writer = &out_writer.interface;
+
+    try std.json.Stringify.value(fhirTypesArr.items, .{ .whitespace = .indent_2 }, writer);
+    try writer.flush();
+
+    const emitted = try emitter.emit(arena, fhirTypesArr);
+
+    std.debug.print("emitted - {d}\n", .{emitted.len});
+
+    var output_dir: std.Io.Dir = try cwd.openDir(initIo, "output", .{});
+    defer output_dir.close(initIo);
+
+    const irFile: std.Io.File = try output_dir.createFile(initIo, "fhir_r5.zig", .{});
     defer irFile.close(initIo);
 
     var file_writer = irFile.writer(initIo, &.{});
